73f105e928a6fd2edb5fd96ff5d011d39a897467
David Harris <dharris@c02ct18jmd6q.desire2learn.d2l>
2022-12-23 14:55:42 -0600

    slimmer constructor

--- flutter-playground/quizzler-flutter/lib/question.dart
+++ flutter-playground/quizzler-flutter/lib/question.dart
@@ -2,11 +2,5 @@ class Question {
   late String text;
   late bool answer;
 
-  Question(
-    String q,
-    bool a,
-  ) {
-    this.text = q;
-    this.answer = a;
-  }
+  Question(this.text, this.answer);
 }
