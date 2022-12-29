2ab3d20522742432db5b9027269fe17933527993
David Harris <dharris@c02ct18jmd6q.desire2learn.d2l>
2022-12-29 10:53:49 -0600

    use http to call api

--- flutter-playground/Clima-Flutter/lib/screens/loading_screen.dart
+++ flutter-playground/Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../services/location.dart';
+
+const kAppId = 'b6907d289e10d714a6e88b30761fae22';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +16,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Latitude: ${location.latitude}, Longitude: ${location.longitude}');
   }
 
+  void getData() async {
+    final double lat = 37.78;
+    final double lon = -122.41;
+    final Uri uri = Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${kAppId}');
+    http.Response response = await http.get(uri);
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +37,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
