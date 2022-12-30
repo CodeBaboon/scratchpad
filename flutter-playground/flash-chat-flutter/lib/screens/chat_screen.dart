72cfc8db34714e2a43fd03ccec3d328768e114f8
David Harris <dharris@c02ct18jmd6q.desire2learn.d2l>
2022-12-30 14:49:06 -0600

    add message stream handling

--- flutter-playground/flash-chat-flutter/lib/screens/chat_screen.dart
+++ flutter-playground/flash-chat-flutter/lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
@@ -21,6 +23,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     getCurrentUser();
+    messagesStream();
   }
 
   void getCurrentUser() {
@@ -35,6 +38,14 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,10 +56,7 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () async {
                 await _auth.signOut();
-                Navigator.pushNamed(
-                  context,
-                  WelcomeScreen.screenId,
-                );
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -59,6 +67,33 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot<dynamic>>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Text> messageWidgets = [];
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+
+                final messages = snapshot.data!.docs;
+
+                for (var message in messages) {
+                  final messageText = message.data()!['text'];
+                  final messageSender = message.data()!['sender'];
+                  final messageWidget =
+                      Text('$messageText from $messageSender');
+                  messageWidgets.add(messageWidget);
+                }
+
+                return Column(
+                  children: messageWidgets,
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
